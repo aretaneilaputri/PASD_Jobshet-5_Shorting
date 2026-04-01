d8fa86a4dd105dd76b6a7d7286485b0a621a57fe
“aretaneilaputri <retaaneila@gmail.com>
2026-04-01 10:16:53 +0700

    5.2 Praktikum 1 - Selection Sort (SortingMain02.java)

--- Praktikum05/SortingMain02.java
+++ Praktikum05/SortingMain02.java
@@ -3,6 +3,7 @@ package Praktikum05;
 public class SortingMain02 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
 
         Sorting02 dataurut1 = new Sorting02(a, a.length);
         System.out.println("Data awal 1");
@@ -10,5 +11,12 @@ public class SortingMain02 {
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        Sorting02 dataurut2 = new Sorting02(b, b.length);
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 }

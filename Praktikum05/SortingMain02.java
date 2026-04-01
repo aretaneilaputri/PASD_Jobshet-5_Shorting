ce513cab8c8702b78d9b97afac2de209ba76d1d8
“aretaneilaputri <retaaneila@gmail.com>
2026-04-01 10:30:31 +0700

    5.2 Praktikum 1 - Insertion Sort (SortingMain02.java)

--- Praktikum05/SortingMain02.java
+++ Praktikum05/SortingMain02.java
@@ -4,6 +4,7 @@ public class SortingMain02 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
         int b[] = {30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
 
         Sorting02 dataurut1 = new Sorting02(a, a.length);
         System.out.println("Data awal 1");
@@ -18,5 +19,12 @@ public class SortingMain02 {
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        Sorting02 dataurut3 = new Sorting02(c, c.length);
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
